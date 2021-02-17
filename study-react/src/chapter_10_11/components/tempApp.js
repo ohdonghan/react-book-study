824b3bdaa986dcb9d76dae2556100a80a74afe10
Donghan <5donghan@naver.com>
2021-02-17 18:32:19 +0900

    tempApp 수정

--- study-react/src/chapter_10_11/components/tempApp.js
+++ study-react/src/chapter_10_11/components/tempApp.js
@@ -3,14 +3,21 @@ import TodoTemplate from "./chapter_10_11/components/TodoTemplate";
 import TodoInsert from "./chapter_10_11/components/TodoInsert";
 import TodoList from "./chapter_10_11/components/TodoList";
 
+const createBulkTodos=()=>{
+    const array=[];
+    for(let i=1;i<=2500;i++){
+        array.push({
+            id:i,
+            text: `할 일 ${i}`,
+            checked: false,
+        });
+    }
+    return array;
+}
 const App=()=>{
-    const [todos,setTodos]=useState([
-        {id:1, text: '할일 1', checked: true},
-        {id:2, text: '할일 2', checked: true},
-        {id:3, text: '할일 3', checked: false},
-    ]);
+    const [todos,setTodos]=useState(createBulkTodos);
 
-    const nextId=useRef(4);
+    const nextId=useRef(2501);
 
     const onInsert=useCallback(text=>{
         const todo={
@@ -18,20 +25,20 @@ const App=()=>{
             text,
             checked:false,
         };
-        setTodos(todos.concat(todo));
+        setTodos(todos=>todos.concat(todo));
         nextId.current+=1;
-    },[todos]);
+    },[]);
 
     const onRemove=useCallback(id=>{
-        setTodos(todos.filter(todo=>todo.id!=id))
-    },[todos])
+        setTodos(todos=>todos.filter(todo=>todo.id!=id))
+    },[])
 
     const onToggle=useCallback(id=>{
-        setTodos(
+        setTodos(todos=>
             todos.map(todo=>
                 todo.id==id?{...todo,checked:!todo.checked}:todo,),
         );
-    },[todos],);
+    },[],);
 
     return <TodoTemplate>
         <TodoInsert onInsert={onInsert}/>
