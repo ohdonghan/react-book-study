dcdff1bc1d90a702f0afd161806467c4a07e42aa
Donghan <5donghan@naver.com>
2021-03-03 11:11:30 +0900

    update tempApp

--- study-react/src/chapter_14/tempApp.js
+++ study-react/src/chapter_14/tempApp.js
@@ -1,1 +1,14 @@
 //const newsAPIKey ='e78e3977b99642cda29e8a0dbfd1701d'
+
+import React,{useState,useCallback} from 'react'
+import {Route} from 'react-router-dom'
+import NewsPage from "./chapter_14/NewsPage";
+
+const App=()=>{
+
+    return(
+        <Route path='/:category?' component={NewsPage}/>
+    )
+}
+
+export default App;
